b9487403d51334c2099c92d614f4c9d8c1727a75
Xinchi Jian <justeriam@gmail.com>
2025-09-05 17:27:41 +0100

    Update my blog system

---
+++ src/main/java/com/hbu/myblog/MyblogApplication.java
@@ -0,0 +1,21 @@
+package com.hbu.myblog;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * SpringBoot 启动类
+ *
+ * @author Vigilr
+ */
+@MapperScan("com.hbu.myblog.mapper")
+@SpringBootApplication
+public class MyblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyblogApplication.class, args);
+		System.out.println("打开浏览器访问 http://localhost:8081/");
+	}
+
+}
